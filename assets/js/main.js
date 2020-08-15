c0a2b87924c09bc7fca2922639c20fbd12d9c58d
Yahaya <yasholma1@gmail.com>
2020-08-15 11:57:08 +0100

    fix arrow up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,39 @@
-$(window).on('scroll', function (e) {
-  if ($(window).scrollTop() > 670) {
-    $('.arrow-up-mobile').fadeIn()
-  } else {
-    $('.arrow-up-mobile').fadeOut()
+// function myFunction(x) {
+//   if (x.matches) {
+//     // If media query matches
+//     $(window).on('scroll', function (e) {
+//       if ($(window).scrollTop() > 670) {
+//         $('.arrow-up-mobile').fadeIn()
+//       } else {
+//         $('.arrow-up-mobile').fadeOut()
+//       }
+//     })
+//   } else {
+//     $('.arrow-up-mobile').fadeOut()
+//   }
+// }
+
+// var x = window.matchMedia('(max-width: 767px)')
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
+
+// media query event handler
+if (matchMedia) {
+  const mq = window.matchMedia('(max-width: 767px)')
+  mq.addListener(WidthChange)
+  WidthChange(mq)
+}
+
+// media query change
+function WidthChange(mq) {
+  console.log(mq)
+  if (mq.matches) {
+    $(window).on('scroll', function (e) {
+      if ($(window).scrollTop() > 670) {
+        $('.arrow-up-mobile').fadeIn()
+      } else {
+        $('.arrow-up-mobile').fadeOut()
+      }
+    })
   }
-})
+}
