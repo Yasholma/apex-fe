426d17c5737f304de6d9d171ed1e6a82ebe936cc
Yahaya <yasholma1@gmail.com>
2020-08-15 17:20:50 +0100

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-// function myFunction(x) {
-//   if (x.matches) {
-//     // If media query matches
-//     $(window).on('scroll', function (e) {
-//       if ($(window).scrollTop() > 670) {
-//         $('.arrow-up-mobile').fadeIn()
-//       } else {
-//         $('.arrow-up-mobile').fadeOut()
-//       }
-//     })
-//   } else {
-//     $('.arrow-up-mobile').fadeOut()
-//   }
-// }
-
-// var x = window.matchMedia('(max-width: 767px)')
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
-
 // media query event handler
 if (matchMedia) {
   const mq = window.matchMedia('(max-width: 767px)')
@@ -26,13 +7,12 @@ if (matchMedia) {
 
 // media query change
 function WidthChange(mq) {
-  console.log(mq)
   if (mq.matches) {
     $(window).on('scroll', function (e) {
       if ($(window).scrollTop() > 670) {
         $('.arrow-up-mobile').fadeIn()
       } else {
-        $('.arrow-up-mobile').fadeOut()
+        $('.arrow-up-mobile').hide()
       }
     })
   }
