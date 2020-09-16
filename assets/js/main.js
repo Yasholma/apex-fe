15d08736be0be10fed9e3556e77a8866030702f8
Yahaya Hyellamoda Yohanna <yasholma1@gmail.com>
2020-09-16 15:36:39 +0100

    add faq page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,34 @@
 // media query event handler
 if (matchMedia) {
-  const mq = window.matchMedia('(max-width: 767px)')
-  mq.addListener(WidthChange)
-  WidthChange(mq)
+  const mq = window.matchMedia("(max-width: 767px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
 }
 
 // media query change
 function WidthChange(mq) {
   if (mq.matches) {
-    $(window).on('scroll', function (e) {
+    $(window).on("scroll", function (e) {
       if ($(window).scrollTop() > 670) {
-        $('.arrow-up-mobile').fadeIn()
+        $(".arrow-up-mobile").fadeIn();
       } else {
-        $('.arrow-up-mobile').hide()
+        $(".arrow-up-mobile").hide();
       }
-    })
+    });
   }
 }
+
+$(function () {
+  let allAccordions = document.querySelectorAll("#faqAccordion button");
+  let allArrowDown = document.querySelectorAll("#faqAccordion .icon");
+
+  allAccordions.forEach((btn, index) => {
+    btn.addEventListener("click", function () {
+      if (this.classList.contains("collapsed")) {
+        allArrowDown[index].classList.add("open");
+      } else {
+        allArrowDown[index].classList.remove("open");
+      }
+    });
+  });
+});
